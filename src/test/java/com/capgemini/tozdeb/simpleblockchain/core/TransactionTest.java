e56f8c5d1bbd8f1f7e1f8d437328add20f2bb385
Tomasz Zdeb <tomasz.zdeb@capgemini.com>
2022-07-19 12:23:55 +0200

    Updated test cases

--- src/test/java/com/capgemini/tozdeb/simpleblockchain/core/TransactionTest.java
+++ src/test/java/com/capgemini/tozdeb/simpleblockchain/core/TransactionTest.java
@@ -12,17 +12,31 @@ public class TransactionTest {
     Transaction transaction;
 
     @Test
-    public void testGetTimestamp() {
+    public void testGetTimestampNonGenesis() {
         long timestamp = Instant.now().getEpochSecond();
         transaction = new Transaction(mockUserA,mockUserB, mockValueA);
         assertTrue(transaction.getTimestamp() >= timestamp);
         assertTrue(transaction.getTimestamp() < timestamp + maxTimestampTimeout);
     }
+    @Test
+    public void testGetTimestampGenesis() {
+        long timestamp = Instant.now().getEpochSecond();
+        transaction = new Transaction(mockUserB, mockValueA);
+        assertTrue(transaction.getTimestamp() >= timestamp);
+        assertTrue(transaction.getTimestamp() < timestamp + maxTimestampTimeout);
+    }
 
     @Test
-    public void testGetSender() {
+    public void testGetSenderNonGenesis() {
         transaction = new Transaction(mockUserA,mockUserB, mockValueA);
         assertEquals(mockUserA,transaction.getSender());
+    }
+    @Test
+    public void testGetSenderGenesis()
+    {
+        transaction = new Transaction(mockUserA,mockValueA);
+
+        assertNull(transaction.getSender());
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -31,41 +45,64 @@ public class TransactionTest {
     }
 
     @Test
-    public void testGetRecipient() {
+    public void testGetRecipientNonGenesis() {
         transaction = new Transaction(mockUserA,mockUserB, mockValueA);
         assertEquals(mockUserB,transaction.getRecipient());
     }
 
+    @Test
+    public void testGetRecipientGenesis() {
+        transaction = new Transaction(mockUserB, mockValueA);
+        assertEquals(mockUserB,transaction.getRecipient());
+    }
+
     @Test(expected = IllegalArgumentException.class)
-    public void testRecipientNullValueThrows() {
+    public void testRecipientNullValueThrowsNonGenesis() {
         transaction = new Transaction(mockUserA,null, mockValueA);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testRecipientNullValueThrowsGenesis() {
+        transaction = new Transaction(null, mockValueA);
+    }
+
     @Test
-    public void testGetAmount(){
+    public void testGetAmountNonGenesis(){
         transaction = new Transaction(mockUserA, mockUserB, mockValueA);
         assertEquals(mockValueA,transaction.getAmount());
     }
 
+    @Test
+    public void testGetAmountGenesis(){
+        transaction = new Transaction(mockUserB, mockValueA);
+        assertEquals(mockValueA,transaction.getAmount());
+    }
+
     @Test(expected = IllegalArgumentException.class)
-    public void testAmountZeroValueThrows(){
+    public void testAmountZeroValueThrowsNonGenesis(){
         final int mockIllegalAmount = 0;
 
         transaction = new Transaction(mockUserA, mockUserB, mockIllegalAmount);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testAmountNegativeValueThrows(){
+    public void testAmountZeroValueThrowsGenesis(){
+        final int mockIllegalAmount = 0;
+
+        transaction = new Transaction(mockUserB, mockIllegalAmount);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAmountNegativeValueThrowsNonGenesis(){
         final int mockIllegalAmount = -1;
 
         transaction = new Transaction(mockUserA, mockUserB, mockIllegalAmount);
     }
 
-    @Test
-    public void testGetGenesisSender()
-    {
-        transaction = new Transaction(mockUserA,mockValueA);
+    @Test(expected = IllegalArgumentException.class)
+    public void testAmountNegativeValueThrowsGenesis(){
+        final int mockIllegalAmount = -1;
 
-        assertNull(transaction.getSender());
+        transaction = new Transaction(mockUserB, mockIllegalAmount);
     }
 }
