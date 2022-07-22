ce58a9e95f5450ad0073cb0b41761a4ca4d6d6c1
Tomasz Zdeb <tomasz.zdeb@capgemini.com>
2022-07-22 16:22:37 +0200

    Added note

--- src/main/java/com/capgemini/tozdeb/simpleblockchain/Main.java
+++ src/main/java/com/capgemini/tozdeb/simpleblockchain/Main.java
@@ -4,4 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    //NOTE - creating interfaces and implementing them by data structure classes could
+    //allow test mocking, so there would be more separation between test classes
 }
