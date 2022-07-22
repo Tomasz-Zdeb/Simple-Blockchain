ca94cfe89fdf1012ced88bc512f9e131f7008dbe
Tomasz Zdeb <tomasz.zdeb@capgemini.com>
2022-07-22 09:58:32 +0200

    Implemented Blockchain class

---
+++ src/main/java/com/capgemini/tozdeb/simpleblockchain/core/Blockchain.java
@@ -0,0 +1,40 @@
+package com.capgemini.tozdeb.simpleblockchain.core;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Blockchain {
+    final long timestamp;
+    final String ID;
+    List<Block> blocks = new ArrayList<>();
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public List<Block> getBlocks() {
+        return blocks;
+    }
+
+    Blockchain(Long timestamp, String ID, List<Block> blocks){
+        this.timestamp = timestamp;
+        this.ID = ID;
+        this.blocks = blocks;
+    }
+
+    Blockchain(String ID, String recipient, int amount){
+        this.timestamp = Instant.now().getEpochSecond();
+        this.ID = ID;
+        blocks.add(new Block(recipient,amount));
+    }
+
+    static Blockchain initializeNewBlockchain(String ID, String recipient, int amount)
+    {
+        return new Blockchain(ID,recipient,amount);
+    }
+}
