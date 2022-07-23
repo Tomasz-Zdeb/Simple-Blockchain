09deeca848219d9a5f0ea8b7f80619cfed96234a
Tomasz Zdeb <tomasz.zdeb@capgemini.com>
2022-07-23 11:34:26 +0200

    Added lacking functionalities

--- src/main/java/com/capgemini/tozdeb/simpleblockchain/core/Blockchain.java
+++ src/main/java/com/capgemini/tozdeb/simpleblockchain/core/Blockchain.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Blockchain {
-    final long timestamp;
-    final String ID;
-    List<Block> blocks = new ArrayList<>();
+    final private long timestamp;
+    final private String ID;
+    private List<Block> blocks = new ArrayList<>();
 
     public long getTimestamp() {
         return timestamp;
@@ -22,6 +22,22 @@ public class Blockchain {
     }
 
     public Blockchain(Long timestamp, String ID, List<Block> blocks){
+        long numberOfGenesisTransactions=0;
+        for(Block block : blocks)
+        {
+            Transaction[] transactions = block.getTransactions();
+            for(Transaction transaction : transactions)
+            {
+                if(transaction != null && transaction.getSender() == null)
+                {
+                    numberOfGenesisTransactions++;
+                }
+            }
+        }
+        if(numberOfGenesisTransactions != 1)
+        {
+            throw new IllegalArgumentException("only one genesis block with one genesis transaction is allowed in a blockchain");
+        }
         this.timestamp = timestamp;
         this.ID = ID;
         this.blocks = blocks;
@@ -36,5 +52,23 @@ public class Blockchain {
     static Blockchain initializeNewBlockchain(String ID, String recipient, int amount)
     {
         return new Blockchain(ID,recipient,amount);
+    }
+
+    public void addBlock(Block block){
+        if(block == null)
+        {
+            throw new IllegalArgumentException("Block to be added can't be null");
+        }
+        long numberOfGenesisTransactions=0;
+        Transaction[] transactions = block.getTransactions();
+        for (Transaction transaction : transactions) {
+            if (transaction != null && transaction.getSender() == null) {
+                numberOfGenesisTransactions++;
+            }
+        }
+        if(numberOfGenesisTransactions>0)
+        {
+            throw new IllegalArgumentException("Cannot add block containing genesis transactions");
+        }
     }
 }
